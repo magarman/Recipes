6456d453ee4a069b8612ed0bed253d55ad50b985
ky82 <barwa@123>
2019-09-17 20:21:12 -0700

    food api script

--- food-api.js
+++ food-api.js
@@ -1,13 +1,14 @@
-var apiKey = "a06d1bc5f6a52079c420fdbf5cc6d9dc";
+
     //quanity on recipes pulled
 
- 
-    $(".-title").click(function () {
+    function addHandler(){
+    $(".calorie-button").click(function () {
      var recipeLabel  = $(".recipe-title").val();
+     console.log(recipeLabel + "khalidtest");
 
 //___________________function to call based on user input_________________//
 //ingredients selected based off user selection 
-    $("#submit").click(function() {
+     {
          var selectedIngredient = $("#dropDownIngredient :selected").val();
         console.log(selectedIngredient);
 
@@ -18,7 +19,7 @@ var apiKey = "a06d1bc5f6a52079c420fdbf5cc6d9dc";
         console.log(selectedAllergy);
    
 
-    var queryURL = "https://api.edamam.com/api/food-database/parser?&app_id=85e120b2&app_key=a06d1bc5f6a52079c420fdbf5cc6d9dc&callback=?&label=" + "&ingr=" + selectedIngredient;
+    var queryURL = "https://api.edamam.com/api/food-database/parser?&app_id=85e120b2&app_key=a06d1bc5f6a52079c420fdbf5cc6d9dc&callback=?&ingr=" + selectedIngredient+ "?&label="+recipeLabel;
 
     //var queryURL = "https://api.edamam.com/api/food-database/parser&ingr=" + selectedIngredient + "&app_id=$95575680&app_key=$a06d1bc5f6a52079c420fdbf5cc6d9dc&from=0&to=10&calories=" + selectedCalorie + "&healthLabels=" + selectedAllergy;
    /// var queryURL ="https://api.edamam.com/api/food-database/parser?&app_id=85e120b2&app_key=a06d1bc5f6a52079c420fdbf5cc6d9dc&ingr=" + selectedIngredient;
@@ -37,7 +38,39 @@ var apiKey = "a06d1bc5f6a52079c420fdbf5cc6d9dc";
                     //following is a test for if outputResults is pulling info and what info it is pulling
                     // console.log(outputResults);
                     // console.log(outputResults.recipe);
-                    console.log(outputResults.food.nutrients)
+                    console.log(outputResults.food.nutrients);
+                    console.log(outputResults.food.nutrients.ENERC_KCAL);
+                    var calorieOutput =  outputResults.food.nutrients;
+                    
+                    var calorieResult = $("<p>").text("Calories: " + calorieOutput);
+                    calorieResult.addClass('calories-display');
+                    $(".calories-display").append(calorieOutput);
+                    $("#workplease").append(calorieResult);
+
+                    console.log(outputResults.food.nutrients.FAT);
+                    var fatOutput = outputResults.food.nutrients.FAT;
+                    var fatResult = $("<p>").text("Fat - " + fatOutput);
+                    fatResult.addClass("fat-display");
+                    $(".fat-display").append(fatOutput);
+                    $("#workplease").append(fatResult);
+                    //console.log(Math.floor(fatOutput));
+
+                    console.log(outputResults.food.nutrients.CHOCDF);
+                   var carbOutput = outputResults.food.nutrients.CHOCDF;
+                   var carbResult = $("<p>").text("Carbs - " + carbOutput);
+                   carbResult.addClass("carb-display");
+                   $(".carb-display").append(carbOutput);
+                   $("#workplease").append(carbResult);
+
+                   console.log(outputResults.food.nutrients.PROCNT);
+                   var proteinOutput = outputResults.food.nutrients.PROCNT;
+                   var proteinResult = $("<p>").text("Protein - " + proteinOutput);
+                   proteinResult.addClass("protein-display");
+                   $(".protein-display").append(proteinOutput);
+                   
+                   $("#workplease").append(proteinResult);
+
+                    //var p= $("<p>").text("Calorie: "+);
                     //stores ingredient output into variable that will be appeneded.
                     console.log(outputResults.food.label);
                     //output for title
@@ -46,12 +79,13 @@ var apiKey = "a06d1bc5f6a52079c420fdbf5cc6d9dc";
                     var nutrientsResults = outputResults.food.nutrients;
                     //output for URL
                     var urlOutputResults = outputResults.measures.uri;
+
+                   
             }
       });
+     }
     });
-    
-    });
-
+    }
 
 
 
